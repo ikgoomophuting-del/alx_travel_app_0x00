8d8f20ec956705840cf9a3271cf137de7faf324f
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-09-21 16:15:30 +0200

    Update serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,1 +1,15 @@
 
+from rest_framework import serializers
+from .models import Listing, Booking
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ["id", "title", "description", "price_per_night", "location", "created_at"]
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ["id", "listing", "user", "check_in", "check_out", "created_at"]
